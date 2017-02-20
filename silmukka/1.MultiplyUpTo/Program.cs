15173a155ec963daf594dc2f865eb930031d71f3
Tommi Saira <tommi.saira@student.saimia.fi>
2017-02-20 11:13:08 +0200

    Fixes ehtorakenne project 5

---
+++ silmukka/1.MultiplyUpTo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.MultiplyUpTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number: ");
+            int result = Convert.ToInt32(Console.ReadLine());
+
+            int[] multiplyTable = new int[];
+            for ()
+        }
+    }
+}
